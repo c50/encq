f8064fd019ecd17602ef0d5d839d966ab87d3a1e
Eric Lemonne <eric@c50.be>
2014-03-06 11:44:47 +0100

    add a toggle submenu navigation function

--- template.php
+++ template.php
@@ -10,3 +10,13 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+ // menus customization
+ // - See more at: http://www.webmar.com.au/blog/how-create-responsive-mobile-menu-omega-subtheme
+function encq_menu_tree__main_menu($variables){
+   if (preg_match("/\bmenu-subnavigation\b/i", $variables['tree'])){
+    return '<ul id="menu-navigation">' . $variables['tree'] . '</ul>';
+  } else {
+    return '<ul class="menu-subnavigation">' . $variables['tree'] . '</ul>';
+  }
+}
