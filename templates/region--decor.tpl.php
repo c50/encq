e4c8af6e844d9e60bc8c69468da9452c6f650b48
Eric Lemonne <eric@c50.be>
2014-03-18 13:28:51 +0100

    add a decoration zone in content

---
+++ templates/region--decor.tpl.php
@@ -0,0 +1,8 @@
+<div<?php print $attributes; ?>>
+  <div<?php print $content_attributes; ?>>
+     <ul>
+	   <li id="neverDie"><img src="/sites/all/themes/encq/images/artists-never-die.png" alt="Artists never die" /></li>
+
+	 </ul>
+	</div>
+</div>
