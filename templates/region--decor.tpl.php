cd3a7286a7c4a27e348e9d65df43dcf956ee35d4
Eric Lemonne <eric@c50.be>
2014-03-19 09:52:07 +0100

    Correct sub-menu positioning while float right option

--- templates/region--decor.tpl.php
+++ templates/region--decor.tpl.php
@@ -2,7 +2,6 @@
   <div<?php print $content_attributes; ?>>
      <ul>
 	   <li id="neverDie"><img src="/sites/all/themes/encq/images/artists-never-die.png" alt="Artists never die" /></li>
-
 	 </ul>
 	</div>
 </div>
