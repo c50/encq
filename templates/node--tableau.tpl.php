d2a4fd2f3589a7e340c2b22ffe03baeec70e1787
Eric Lemonne <eric@c50.be>
2014-03-19 01:49:12 +0100

    Corrections node tableau

--- templates/node--tableau.tpl.php
+++ templates/node--tableau.tpl.php
@@ -3,7 +3,7 @@
   
   <header>
     <?php print render($title_prefix); ?>
-    <h2<?php print $title_attributes; ?>><?php print $title?></h2>
+    <!-- <h2<?php print $title_attributes; ?>><?php print $title?></h2> -->
     <?php print render($title_suffix); ?>
   </header>
   
