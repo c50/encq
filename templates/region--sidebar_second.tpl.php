20eda2fbbc224a633b55a41906e58745d5fa9bc1
Eric Lemonne <eric@c50.be>
2014-03-23 10:44:05 +0100

    add logo option compatibility

---
+++ templates/region--sidebar_second.tpl.php
@@ -0,0 +1,6 @@
+<aside<?php print $attributes; ?>>
+  <a href="/" rel="home"><img class="logoColumn" src="/sites/all/themes/encq/images/logo-encq-small.jpg" alt="logo encq" /></a>
+  <br /><div<?php print $content_attributes; ?>>
+    <?php print $content; ?>
+  </div>
+</aside>
