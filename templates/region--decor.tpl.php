794f3e01803b6cc959f8fd9c1d1ccb53479a9427
Eric Lemonne <eric@c50.be>
2014-03-23 11:42:12 +0100

    Better text formatting for copyright

--- templates/region--decor.tpl.php
+++ templates/region--decor.tpl.php
@@ -2,7 +2,7 @@
   <div<?php print $content_attributes; ?>>
      <ul id="decor">
 	   <li id="neverDie"><img src="/sites/all/themes/encq/images/artists-never-die.png" alt="Artists never die" /></li>
-	   <li id="copy">copyright encq &copy; 2014</li>
+	   <li id="copy">copyright Encq &copy;2014</li>
 	 </ul>
 	</div>
 </div>
