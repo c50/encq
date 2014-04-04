1d293c77a1567dcd8b061492ca6cb74fde66f152
Eric Lemonne <eric@c50.be>
2014-04-04 16:50:46 +0200

    Meta descriptions in nl/en plus text style for colophon

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -3,7 +3,11 @@
 <head>
 <title><?php print $head_title; ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
 <meta name="description" lang="fr" content="Peintre bruxellois passionné par l'humain">
+<meta name="description" lang="nl" content="Brussels schilder gefascineerd door de mens ">
+<meta name="description" lang="en" content="Brussels painter fascibated by human">
+
 <meta name="keywords" content="encq, artiste peintre, tableau acrylique, atelier de peinture Bruxelles">
 <meta name="author" content="Eric Lemonne, C50 Dynamic Design">
 <meta name="identifier-url" content="http://www.encq.be">
@@ -19,7 +23,20 @@
   <script type="text/javascript" src="//use.typekit.net/ktd5qnd.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3472351-25']);
+  _gaq.push(['_setDomainName', 'encq.be']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 
 </head>
 <body<?php print $attributes;?>>
