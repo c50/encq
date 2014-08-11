e3395077c1ec802923257d9d93d44c7c422e425a
Eric Lemonne <eric@c50.be>
2014-08-11 17:25:33 +0200

    Revert "correct colophon"
    This reverts commit f7f393c4d1888594b8c7d7aca7a3e3af1ae284db.

--- templates/region--footer-c50.tpl.php
+++ templates/region--footer-c50.tpl.php
@@ -1,6 +1,6 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
-     <p><abbr><a class="discreet" target="_blank" href="http://www.c50.be">Designed by C50 Dynamic Design</a></abbr>
+     <p><abbr><a class="discreet" target="_blank" href="http://www.c50.be">Designed by C50-Dynamic Design</a></abbr>
 	<br /><abbr><a class="discreet" target="_blank" href="http://drupal.org/project/omega" style="font-size:0.8em;top:2em;">Another Omega Responsive HTML5 subtheme</a></abbr></p>
 		  <?php print $content; ?>
 </div>
